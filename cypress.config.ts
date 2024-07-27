5638c5ac2c3a71a53aec603013a731857c7eefa3
Naman Jain <namjain0@WKMZT2840F9A.global.publicisgroupe.net>
2024-07-27 13:22:15 +0530

    latest changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "cypress";
+const { installPlugin } = require("@chromatic-com/cypress");
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      installPlugin(on, config);
     },
   },
 });
--- cypress/e2e/product-listing.cy.ts
+++ cypress/e2e/product-listing.cy.ts
@@ -1,5 +1,17 @@
-describe('template spec', () => {
+describe('product-listing spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:5173')
+    cy.intercept("GET", "https://fakestoreapi.com/products", { fixture: 'products.json' }).as("products");
+
+    cy.visit('http://localhost:5173');
+
+    cy.wait("@products").its('response.statusCode').should('eq', 200);
+
+    cy.get('img').each(img => {
+        cy.wrap(img)
+          .scrollIntoView()
+          .its('0.naturalWidth')   // Note "0" extracts element from jQuery wrapper
+          .should('be.greaterThan', 0)
+      });
+
   })
 })
